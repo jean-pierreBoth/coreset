ea094a71e9fbd6c7f29d140e0d17e124a6c63c61
jean-pierreBoth <jeanpierre.both@gmail.com>
2024-04-30 14:32:15 +0200

    hnswcore...

--- fromhnsw/src/bin/hnswcore.rs
+++ fromhnsw/src/bin/hnswcore.rs
@@ -13,6 +13,42 @@ use std::time::{Duration, SystemTime};
 use coreset::prelude::*;
 use std::iter::Iterator;
 
+use std::default::Default;
+
+//========================================
+
+/// Coreset parameters
+struct CoresetParams {
+    beta: f32,
+    gamma: f32,
+}
+
+impl CoresetParams {
+    fn new(beta: f32, gamma: f32) -> CoresetParams {
+        CoresetParams { beta, gamma }
+    }
+    //
+    fn get_beta(&self) -> f32 {
+        self.beta
+    }
+
+    //
+    fn get_gamma(&self) -> f32 {
+        self.gamma
+    }
+}
+
+impl Default for CoresetParams {
+    fn default() -> Self {
+        CoresetParams {
+            beta: 2.,
+            gamma: 2.,
+        }
+    }
+}
+
+//==========================================
+
 fn main() {
     //
     let _ = env_logger::builder().is_test(true).try_init();
